ad18ab290151d97bac6ba137e7b8a03a56686bdc
NguyenQuangLeKhoi <0306231120@caothang.edu.vn>
2026-01-16 23:17:34 +0700

    Them api tao cookbook, controllercookbook,

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Api\CookbookController;
 use Illuminate\Support\Facades\Route;
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+//Khôi
+Route::post('/cookbook/create', [CookbookController::class, 'store']);
